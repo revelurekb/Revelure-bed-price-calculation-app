a002c4f6682183073e8a1eabd85b96377a152538
revelurekb <revelurekb@gmail.com>
2025-12-15 16:36:00 +0530

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,11 @@
-const CACHE_NAME = 'bed-calc-v1';
-const BASE_PATH = '/Revelure-bed-price-calculation-app/';
-
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        BASE_PATH,
-        BASE_PATH + 'index.html',
-        BASE_PATH + 'image.png',
-        BASE_PATH + 'icon-192.png',
-        BASE_PATH + 'icon-512.png'
-      ]);
-    })
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
